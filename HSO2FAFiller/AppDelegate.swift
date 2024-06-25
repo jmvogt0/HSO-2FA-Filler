5bc8430f4a888318f500f6fbf74b5d88de77714b
Jan Vogt <Jan.vogt2000@icloud.com>
2024-06-25 22:35:30 +0200

    Initial Commit

---
+++ HSO2FAFiller/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  HSO2FAFiller
+//
+//  Created by Jan Vogt on 25.06.24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
